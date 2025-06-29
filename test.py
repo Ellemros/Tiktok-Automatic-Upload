fc82dcbcfa258d49c8a7281038ddaab298613a1b
Ellemros <ellemros@seznam.cz>
2025-06-29 16:34:10 +0200

    nc

---
+++ test.py
@@ -0,0 +1,5 @@
+from tiktok_uploader.upload import upload_video, upload_videos
+from tiktok_uploader.auth import AuthBackend
+
+# single video
+upload_video('Download.mp4', description='My first video #AI#AIGenerated', cookies='cookies.txt')
